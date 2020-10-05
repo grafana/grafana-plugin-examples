81aebf8b5c722074f6257ec9d9f6d681e86bb3d5
Marcus Olsson <marcus.olsson@hey.com>
2020-10-05 11:43:15 +0200

    Add visx example

---
+++ visx/src/SimplePanel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { PanelProps, DataFrameView, FieldType } from '@grafana/data';
+import { SimpleOptions } from 'types';
+
+import { AreaClosed } from '@vx/shape';
+import { scaleTime, scaleLinear } from '@vx/scale';
+
+interface Props extends PanelProps<SimpleOptions> {}
+
+interface DataPoint {
+  Time: number;
+  Value: number;
+}
+
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const { from, to } = data.timeRange;
+
+  const frame = data.series[0];
+  const valueField = frame.fields.find(f => f.type === FieldType.number);
+
+  const view = new DataFrameView<DataPoint>(frame).toArray();
+
+  const getDate = (d: DataPoint) => new Date(d.Time);
+  const getValue = (d: DataPoint) => d.Value;
+
+  const dateScale = scaleTime({
+    range: [0, width],
+    domain: [from.toDate(), to.toDate()],
+  });
+
+  const valueScale = scaleLinear({
+    range: [height, 0],
+    domain: [valueField?.config.min || 0, valueField?.config.max || 0],
+    nice: true,
+  });
+
+  return (
+    <div style={{ width, height }}>
+      <svg width={width} height={height}>
+        <AreaClosed
+          data={view}
+          x={d => dateScale(getDate(d)) || 0}
+          y={d => valueScale(getValue(d)) || 0}
+          yScale={valueScale}
+          fill={'red'}
+        />
+      </svg>
+    </div>
+  );
+};
