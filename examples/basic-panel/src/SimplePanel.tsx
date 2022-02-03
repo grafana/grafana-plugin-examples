978461934023c0bc5ae4405d789617294f701ff1
Jack Westbrook <jack.westbrook@gmail.com>
2022-02-03 17:13:46 +0100

    feat(basic-panel): add config, timeRange and onChangeTimeRange to example

--- examples/basic-panel/src/SimplePanel.tsx
+++ examples/basic-panel/src/SimplePanel.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
-import { TimeSeries } from '@grafana/ui';
+import { TimeSeries, TooltipPlugin, TooltipDisplayMode, ZoomPlugin } from '@grafana/ui';
 import { SimpleOptions } from 'types';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
-export function SimplePanel({ options, data, width, height, timeRange, timeZone }: Props) {
+export function SimplePanel({ options, data, width, height, timeZone, timeRange, onChangeTimeRange }: Props) {
   return (
     <TimeSeries
       width={width}
@@ -14,6 +14,20 @@ export function SimplePanel({ options, data, width, height, timeRange, timeZone 
       timeZone={timeZone}
       frames={data.series}
       legend={options.legend}
-    />
+    >
+      {(config, alignedDataFrame) => {
+        return (
+          <>
+            <TooltipPlugin
+              config={config}
+              data={alignedDataFrame}
+              mode={TooltipDisplayMode.Multi}
+              timeZone={timeZone}
+            />
+            <ZoomPlugin config={config} onZoom={onChangeTimeRange} />
+          </>
+        );
+      }}
+    </TimeSeries>
   );
 }
