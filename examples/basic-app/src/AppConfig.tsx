c415c3e58827522facab83c83437b05e8901d210
Levente Balogh <balogh.levente.hu@gmail.com>
2022-02-10 13:22:10 +0100

    feat: add example for saving custom data for a plugin

--- examples/basic-app/src/AppConfig.tsx
+++ examples/basic-app/src/AppConfig.tsx
@@ -1,19 +1,61 @@
-import React from 'react';
-import { Button, Legend, useStyles2 } from '@grafana/ui';
+import React, { useState, ChangeEvent } from 'react';
+import { Button, Legend, Field, Input, useStyles2 } from '@grafana/ui';
 import { PluginConfigPageProps, AppPluginMeta, PluginMeta, GrafanaTheme2 } from '@grafana/data';
 import { getBackendSrv } from '@grafana/runtime';
 import { css } from '@emotion/css';
+import { SecretInput } from '../src/components/SecretInput';
 
-export type AppPluginSettings = {};
+export type JsonData = {
+  apiUrl?: string;
+  isApiKeySet?: boolean;
+};
 
-interface Props extends PluginConfigPageProps<AppPluginMeta<AppPluginSettings>> {}
+type State = {
+  // The URL to reach our custom API.
+  apiUrl: string;
+  // Tells us if the API key secret is set.
+  // Set to `true` ONLY if it has already been set and haven't been changed.
+  // (We unfortunately need an auxiliray variable for this, as `secureJsonData` is never exposed to the browser after it is set)
+  isApiKeySet: boolean;
+  // An secret key for our custom API.
+  apiKey: string;
+};
+
+interface Props extends PluginConfigPageProps<AppPluginMeta<JsonData>> {}
 
 export const AppConfig = ({ plugin }: Props) => {
   const s = useStyles2(getStyles);
-  const { enabled, jsonData } = plugin.meta;
+  const { enabled, pinned, jsonData } = plugin.meta;
+  const [state, setState] = useState<State>({
+    apiUrl: jsonData?.apiUrl || '',
+    apiKey: '',
+    isApiKeySet: Boolean(jsonData?.isApiKeySet),
+  });
+
+  const onResetApiKey = () =>
+    setState({
+      ...state,
+      apiKey: '',
+      isApiKeySet: false,
+    });
+
+  const onChangeApiKey = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      apiKey: event.target.value.trim(),
+    });
+  };
+
+  const onChangeApiUrl = (event: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      apiUrl: event.target.value.trim(),
+    });
+  };
 
   return (
     <div className="gf-form-group">
+      {/* ENABLE / DISABLE PLUGIN */}
       <div>
         {/* Enable the plugin */}
         <Legend>Enable / Disable</Legend>
@@ -56,6 +98,65 @@ export const AppConfig = ({ plugin }: Props) => {
           </>
         )}
       </div>
+
+      {/* CUSTOM SETTINGS */}
+      <div className={s.marginTopXl}>
+        <Legend>API Settings</Legend>
+
+        {/* API Key */}
+        <Field label="API Key" description="A secret key for authenticating to our custom API">
+          <SecretInput
+            className={s.input}
+            data-testid="api-key"
+            id="api-key"
+            value={state?.apiKey}
+            isConfigured={state.isApiKeySet}
+            placeholder={'Your secret API key'}
+            onChange={onChangeApiKey}
+            onReset={onResetApiKey}
+          />
+        </Field>
+
+        {/* API Url */}
+        <Field label="API Url" description="" className={s.marginTop}>
+          <Input
+            css=""
+            className={s.input}
+            id="api-url"
+            data-testid="api-url"
+            label={`API Url`}
+            value={state?.apiUrl}
+            placeholder={`E.g.: http://mywebsite.com/api/v1`}
+            onChange={onChangeApiUrl}
+          />
+        </Field>
+
+        <div className={s.marginTop}>
+          <Button
+            type="submit"
+            onClick={() =>
+              updatePluginAndReload(plugin.meta.id, {
+                enabled,
+                pinned,
+                jsonData: {
+                  apiUrl: state.apiUrl,
+                  isApiKeySet: true,
+                },
+                // This cannot be queried later by the frontend.
+                // We don't want to override it in case it was set previously and left untouched now.
+                secureJsonData: state.isApiKeySet
+                  ? undefined
+                  : {
+                      apiKey: state.apiKey,
+                    },
+              })
+            }
+            disabled={Boolean(!state.apiUrl || (!state.isApiKeySet && !state.apiKey))}
+          >
+            Save API settings
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
@@ -67,9 +168,15 @@ const getStyles = (theme: GrafanaTheme2) => ({
   marginTop: css`
     margin-top: ${theme.spacing(3)};
   `,
+  marginTopXl: css`
+    margin-top: ${theme.spacing(6)};
+  `,
+  input: css`
+    width: 500px;
+  `,
 });
 
-const updatePluginAndReload = async (pluginId: string, data: Partial<PluginMeta>) => {
+const updatePluginAndReload = async (pluginId: string, data: Partial<PluginMeta<JsonData>>) => {
   try {
     await updatePlugin(pluginId, data);
 
