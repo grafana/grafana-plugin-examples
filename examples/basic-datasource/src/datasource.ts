d9cb315e8e7edd9f0ef028f65d2d121a745b59da
Marcus Andersson <marcus.andersson@grafana.com>
2022-02-10 12:03:44 +0100

    added support for template variables.

--- examples/basic-datasource/src/datasource.ts
+++ examples/basic-datasource/src/datasource.ts
@@ -1,10 +1,17 @@
-import { DataSourceWithBackend } from '@grafana/runtime';
-import type { DataSourceInstanceSettings } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import type { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
 import type { BasicQuery, BasicDataSourceOptions, QueryTypesResponse } from './types';
 
 export class BasicDataSource extends DataSourceWithBackend<BasicQuery, BasicDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<BasicDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: BasicQuery, scopedVars: ScopedVars): Record<string, any> {
+    return {
+      ...query,
+      rawQuery: getTemplateSrv().replace(query.rawQuery, scopedVars),
+    };
   }
 
   getAvailableQueryTypes(): Promise<QueryTypesResponse> {
