7646841a8202c5eab3a60ebbd8ecb87a8f0f5e5a
Marcus Andersson <marcus.andersson@grafana.com>
2022-02-17 15:26:47 +0100

    tests for configuration editor.

--- examples/datasource-basic/cypress/integration/config.spec.ts
+++ examples/datasource-basic/cypress/integration/config.spec.ts
@@ -1,4 +1,34 @@
 import { e2e } from '@grafana/e2e';
 
-describe('configurating datasource', () => {
+const configEditor = e2e.getSelectors({
+    timeField: 'input[aria-label="Macro default time field"]',
+    apiKey: 'input[aria-label="Secret api key"]',
+});
+
+describe.only('configurating datasource', () => {
+  it('should be possible to update api key and default time field', () => {
+    e2e.flows.addDataSource({
+      type: 'Basic Datasource',
+      form: () => {
+        configEditor.timeField().type('created');
+        configEditor.apiKey().type('super-secret-api-key');
+      },
+    });
+
+    configEditor.timeField().should('have.value', 'created');
+    configEditor.apiKey().should('have.value', 'configured');
+  });
+
+  it('should be possible to reset the api key', () => {
+    e2e.flows.addDataSource({
+      type: 'Basic Datasource',
+      form: () => {
+        configEditor.timeField().type('created');
+        configEditor.apiKey().type('super-secret-api-key');
+      },
+    });
+
+    configEditor.apiKey().siblings('button').click();
+    configEditor.apiKey().should('have.value', '');
+  });
 });
--- examples/datasource-basic/src/components/ConfigEditor/ConfigEditor.tsx
+++ examples/datasource-basic/src/components/ConfigEditor/ConfigEditor.tsx
@@ -8,7 +8,7 @@ import { useResetSecureOptions } from './useResetSecureOptions';
 const { SecretFormField } = LegacyForms;
 
 export function ConfigEditor(props: EditorProps): ReactElement {
-  const { jsonData, secureJsonData } = props.options;
+  const { jsonData, secureJsonData, secureJsonFields } = props.options;
   const onTimeFieldChange = useChangeOptions(props, 'defaultTimeField');
   const onApiKeyChange = useChangeSecureOptions(props, 'apiKey');
   const onResetApiKey = useResetSecureOptions(props, 'apiKey');
@@ -17,19 +17,25 @@ export function ConfigEditor(props: EditorProps): ReactElement {
     <>
       <FieldSet label="General">
         <InlineField label="Time Field" tooltip="Default time field used when interpolate the $__timeFilter()">
-          <Input onChange={onTimeFieldChange} placeholder="time" value={jsonData?.defaultTimeField ?? ''} />
+          <Input
+            onChange={onTimeFieldChange}
+            placeholder="time"
+            aria-label="Macro default time field"
+            value={jsonData?.defaultTimeField ?? ''}
+          />
         </InlineField>
       </FieldSet>
 
       <FieldSet label="API Settings">
         <SecretFormField
           tooltip="API Key used to make calls to your data source"
-          isConfigured={Boolean(secureJsonData?.apiKey)}
+          isConfigured={Boolean(secureJsonFields['apiKey'])}
           value={secureJsonData?.apiKey || ''}
           label="API Key"
           placeholder="secure json field (backend only)"
           labelWidth={6}
           inputWidth={20}
+          aria-label="Secret api key"
           onReset={onResetApiKey}
           onChange={onApiKeyChange}
         />
