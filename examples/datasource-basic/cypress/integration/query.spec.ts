3808c3759b4f38309297753e729f829d36c14de5
Marcus Andersson <marcus.andersson@grafana.com>
2022-02-21 19:05:42 +0100

    removing tests.

--- examples/datasource-basic/cypress/integration/query.spec.ts
+++ examples/datasource-basic/cypress/integration/query.spec.ts
@@ -12,71 +12,71 @@ describe('querying datasource', () => {
     });
   });
 
-  describe('with query type: time series', () => {
-    it.skip('should return data with one value/time field', () => {
-      const panel = 'Timeseries';
-      const data = [
-        { time: '2022-02-01 10:00:00', value: 25 },
-        { time: '2022-02-01 10:12:00', value: 22 },
-        { time: '2022-02-01 10:24:00', value: 19 },
-        { time: '2022-02-01 10:36:00', value: 23 },
-        { time: '2022-02-01 10:48:00', value: 22 },
-        { time: '2022-02-01 11:00:00', value: 22 },
-        { time: '2022-02-01 11:12:00', value: 18 },
-        { time: '2022-02-01 11:24:00', value: 26 },
-        { time: '2022-02-01 11:36:00', value: 24 },
-        { time: '2022-02-01 11:48:00', value: 20 },
-      ];
+  // describe('with query type: time series', () => {
+  //   it('should return data with one value/time field', () => {
+  //     const panel = 'Timeseries';
+  //     const data = [
+  //       { time: '2022-02-01 10:00:00', value: 25 },
+  //       { time: '2022-02-01 10:12:00', value: 22 },
+  //       { time: '2022-02-01 10:24:00', value: 19 },
+  //       { time: '2022-02-01 10:36:00', value: 23 },
+  //       { time: '2022-02-01 10:48:00', value: 22 },
+  //       { time: '2022-02-01 11:00:00', value: 22 },
+  //       { time: '2022-02-01 11:12:00', value: 18 },
+  //       { time: '2022-02-01 11:24:00', value: 26 },
+  //       { time: '2022-02-01 11:36:00', value: 24 },
+  //       { time: '2022-02-01 11:48:00', value: 20 },
+  //     ];
 
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.PanelInspector.Data.content()
-        .should('be.visible')
-        .within(() => {
-          for (let index = 0; index < data.length; index++) {
-            const expected = data[index];
-            const timeIndex = index * 2;
-            const valueIndex = timeIndex + 1;
+  //     e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
+  //     e2e.components.PanelInspector.Data.content()
+  //       .should('be.visible')
+  //       .within(() => {
+  //         for (let index = 0; index < data.length; index++) {
+  //           const expected = data[index];
+  //           const timeIndex = index * 2;
+  //           const valueIndex = timeIndex + 1;
 
-            cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
-            cy.get('div[role=cell]').eq(valueIndex).contains(expected.value).should('be.visible');
-          }
-        });
-    });
-  });
+  //           cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
+  //           cy.get('div[role=cell]').eq(valueIndex).contains(expected.value).should('be.visible');
+  //         }
+  //       });
+  //   });
+  // });
 
-  describe('with query type: table', () => {
-    it.skip('should return data with time and two value fields', () => {
-      const panel = 'Table';
-      const data = [
-        { time: '2022-02-01 10:00:00', inside: 25, outside: 10 },
-        { time: '2022-02-01 10:12:00', inside: 22, outside: 8 },
-        { time: '2022-02-01 10:24:00', inside: 19, outside: 12 },
-        { time: '2022-02-01 10:36:00', inside: 23, outside: 9 },
-        { time: '2022-02-01 10:48:00', inside: 22, outside: 10 },
-        { time: '2022-02-01 11:00:00', inside: 22, outside: 11 },
-        { time: '2022-02-01 11:12:00', inside: 18, outside: 10 },
-        { time: '2022-02-01 11:24:00', inside: 26, outside: 9 },
-        { time: '2022-02-01 11:36:00', inside: 24, outside: 10 },
-        { time: '2022-02-01 11:48:00', inside: 20, outside: 9 },
-      ];
+  // describe('with query type: table', () => {
+  //   it('should return data with time and two value fields', () => {
+  //     const panel = 'Table';
+  //     const data = [
+  //       { time: '2022-02-01 10:00:00', inside: 25, outside: 10 },
+  //       { time: '2022-02-01 10:12:00', inside: 22, outside: 8 },
+  //       { time: '2022-02-01 10:24:00', inside: 19, outside: 12 },
+  //       { time: '2022-02-01 10:36:00', inside: 23, outside: 9 },
+  //       { time: '2022-02-01 10:48:00', inside: 22, outside: 10 },
+  //       { time: '2022-02-01 11:00:00', inside: 22, outside: 11 },
+  //       { time: '2022-02-01 11:12:00', inside: 18, outside: 10 },
+  //       { time: '2022-02-01 11:24:00', inside: 26, outside: 9 },
+  //       { time: '2022-02-01 11:36:00', inside: 24, outside: 10 },
+  //       { time: '2022-02-01 11:48:00', inside: 20, outside: 9 },
+  //     ];
 
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.PanelInspector.Data.content()
-        .should('be.visible')
-        .within(() => {
-          for (let index = 0; index < data.length; index++) {
-            const expected = data[index];
-            const timeIndex = index * 3;
-            const outsideIndex = timeIndex + 1;
-            const insideIndex = outsideIndex + 1;
+  //     e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
+  //     e2e.components.PanelInspector.Data.content()
+  //       .should('be.visible')
+  //       .within(() => {
+  //         for (let index = 0; index < data.length; index++) {
+  //           const expected = data[index];
+  //           const timeIndex = index * 3;
+  //           const outsideIndex = timeIndex + 1;
+  //           const insideIndex = outsideIndex + 1;
 
-            cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
-            cy.get('div[role=cell]').eq(outsideIndex).contains(expected.outside).should('be.visible');
-            cy.get('div[role=cell]').eq(insideIndex).contains(expected.inside).should('be.visible');
-          }
-        });
-    });
-  });
+  //           cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
+  //           cy.get('div[role=cell]').eq(outsideIndex).contains(expected.outside).should('be.visible');
+  //           cy.get('div[role=cell]').eq(insideIndex).contains(expected.inside).should('be.visible');
+  //         }
+  //       });
+  //   });
+  // });
 
   describe('with macro: $__timeFilter()', () => {
     it('should use default time column in query', () => {
