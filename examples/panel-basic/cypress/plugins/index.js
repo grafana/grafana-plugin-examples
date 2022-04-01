005a89d663c623ae6b805a841ca1fb080d99573b
Marcus Andersson <marcus.andersson@grafana.com>
2022-04-01 13:56:08 +0200

    trying to force color profile via env

--- examples/panel-basic/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (on, config) => {
-  // making sure we use the same color profile regardless where we run
-  // the tests.
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    if (browser.family === 'chromium' && browser.name !== 'electron') {
-      launchOptions.args.push('--force-color-profile=srgb');
-    }
-  });
-};
