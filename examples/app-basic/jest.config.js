3c6dca64036a2edfa5ca5d51de85e35e8155ff76
Levente Balogh <balogh.levente.hu@gmail.com>
2022-05-30 09:16:33 +0200

    fix: update the Jest config

--- examples/app-basic/jest.config.js
+++ examples/app-basic/jest.config.js
@@ -1,17 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}',
-  ],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-    },
-  },
+  // Jest configuration provided by Grafana scaffolding
+  ...require('./.config/jest.config'),
 };
