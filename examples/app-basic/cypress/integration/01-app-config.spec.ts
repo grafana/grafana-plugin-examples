cab6620b9a124e80b03c3b24950cff50dec5dc08
Marcus Andersson <marcus.andersson@grafana.com>
2022-09-21 13:26:44 +0200

    fixed failing tests.

--- examples/app-basic/cypress/integration/01-app-config.spec.ts
+++ examples/app-basic/cypress/integration/01-app-config.spec.ts
@@ -6,7 +6,7 @@ const { appConfig } = e2e.getSelectors(testIds);
 
 describe('configurating app', () => {
   beforeEach(() => {
-    cy.visit(`http://localhost:3000/a/${pluginJson.id}`);    
+    cy.visit(`http://localhost:3000/plugins/${pluginJson.id}`);    
   });
 
   it('should be successfully configured', () => {
