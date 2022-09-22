b8b066201ab847fb2b526d77a3c98ca570ff0277
Jack Westbrook <jack.westbrook@gmail.com>
2022-09-22 12:06:19 +0200

    test(datalinks): add e2e tests for datalinks

--- examples/panel-datalinks/cypress/integration/02-edit-panel.spec.ts
+++ examples/panel-datalinks/cypress/integration/02-edit-panel.spec.ts
@@ -1,7 +1,5 @@
 import { e2e } from '@grafana/e2e';
-import { ariaLabels } from '../../src/components/ariaLabels';
 
-const selectors = e2e.getSelectors(ariaLabels);
 const panel = 'Datalinks';
 
 describe('editing a panel with time series data', () => {
@@ -11,26 +9,11 @@ describe('editing a panel with time series data', () => {
     });
   });
 
-  it('should be able to add datalinks to the panel', () => {
-    e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, panel);
-
-    e2e.components.OptionsGroup.group('Data links')
-      .scrollIntoView()
-      .within(() => {
-        selectors.dataLinksEditor().find('button').contains('Add link').click();
-      });
-
-    cy.get('div[data-overlay-container="true"]').within(() => {
-      cy.get('input[placeholder="Show details"]').type('Visit Bing');
-      cy.get('div[data-slate-editor="true"]').focus().type('h').wait(1000).type('http://www.bing.com');
-      cy.get('button').contains('Save').click();
-    });
-
-    e2e.components.PanelEditor.applyButton().click();
-
+  it('should have datalinks available in the panel', () => {
     e2e.components.Panels.Panel.containerByTitle(panel)
       .should('be.visible')
       .find('.panel-content')
+      .should('be.visible')
       .get('svg')
       .find('circle')
       .first()
@@ -41,59 +24,14 @@ describe('editing a panel with time series data', () => {
       .should('be.visible')
       .parent()
       .find('a')
-      .should('have.length', 3);
+      .should('have.length', 2)
+      .first()
+      .should('contain', 'Visit Yahoo')
+      .should('have.attr', 'rel', 'noopener noreferrer')
+      .should('have.attr', 'href', 'https://www.yahoo.com')
+      .next()
+      .should('contain', 'Visit Google')
+      .should('have.attr', 'rel', 'noopener noreferrer')
+      .should('have.attr', 'href', 'https://www.google.com');
   });
-
-  // it('should be able to change graph gradient', () => {
-  //   e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, panel);
-  // e2e.components.PanelEditor.OptionsPane.content()
-  //   .should('be.visible')
-  //   .within(() => {
-  //     selectors.gradientScheme().should('be.checked');
-  //     selectors.gradientHue().check({ force: true }).should('be.checked');
-  //   });
-
-  //   e2e.components.PanelEditor.applyButton().click();
-
-  //   e2e.components.Panels.Panel.containerByTitle(panel).should('be.visible').find('.panel-content');
-  // });
-
-  // it('should be able to change graph opacity', () => {
-  //   e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, panel);
-  //   e2e.components.PanelEditor.OptionsPane.content()
-  //     .should('be.visible')
-  //     .within(() => {
-  //       selectors.fillOpacity().click().focused().clear().type('100');
-  //     });
-
-  //   e2e.components.PanelEditor.applyButton().click();
-
-  //   e2e.components.Panels.Panel.containerByTitle(panel).should('be.visible').find('.panel-content');
-  // });
-
-  // it('should be able to hide legend', () => {
-  //   e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, panel);
-  //   e2e.components.PanelEditor.OptionsPane.content()
-  //     .should('be.visible')
-  //     .within(() => {
-  //       selectors.legendDisplayHidden().check({ force: true }).should('be.checked');
-  //     });
-
-  //   e2e.components.PanelEditor.applyButton().click();
-
-  //   e2e.components.Panels.Panel.containerByTitle(panel).should('be.visible').find('.panel-content');
-  // });
-
-  // it('should be able to place legend to the right', () => {
-  //   e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Edit, panel);
-  //   e2e.components.PanelEditor.OptionsPane.content()
-  //     .should('be.visible')
-  //     .within(() => {
-  //       selectors.legendPlacementRight().check({ force: true }).should('be.checked');
-  //     });
-
-  //   e2e.components.PanelEditor.applyButton().click();
-
-  //   e2e.components.Panels.Panel.containerByTitle(panel).should('be.visible').find('.panel-content');
-  // });
 });
--- examples/panel-datalinks/src/components/ariaLabels.ts
+++
@@ -1,4 +0,0 @@
-export const ariaLabels = {
-  dataLinksPane: 'Options group Data links',
-  dataLinksEditor: 'Data links Data links field property editor',
-};
