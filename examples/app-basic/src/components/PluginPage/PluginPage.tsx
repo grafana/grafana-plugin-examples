b58a67ecd5813055126ac91241b5ec91711bbebf
Levente Balogh <balogh.levente.hu@gmail.com>
2022-09-27 14:35:16 +0200

    Wip

--- examples/app-basic/src/components/PluginPage/PluginPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { PluginPageProps, PluginPage as GrafanaPluginPage, config } from '@grafana/runtime';
-
-const PluginPageFallback = ({ children }: PluginPageProps) => <>{children}</>;
-
-export const PluginPage = GrafanaPluginPage && config.featureToggles.topnav ? GrafanaPluginPage : PluginPageFallback;
