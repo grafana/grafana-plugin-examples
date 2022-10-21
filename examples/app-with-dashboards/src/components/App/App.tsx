4ba018976033943520703491e0ed1a830711ea79
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2022-10-21 17:28:00 +0200

    Adding app example with dashboards

--- examples/app-with-dashboards/src/components/App/App.tsx
+++ examples/app-with-dashboards/src/components/App/App.tsx
@@ -3,6 +3,22 @@ import { AppRootProps } from '@grafana/data';
 
 export class App extends React.PureComponent<AppRootProps> {
   render() {
-    return <div className="page-container">Hello Grafana!</div>;
+    return (
+      <>
+        <div className="page-container">
+          <h1>Hello and welcome to our demo app!</h1>
+          <h2>Check out our dashboards:</h2>
+          <br />
+          <ul>
+            <li>
+              <a href="/d/Av57mRHVz">Example dashboard 1</a>
+            </li>
+            <li>
+              <a href="/d/ND1Bfw3VcNGg">Example dashboard 2</a>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
 }
