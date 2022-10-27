1e13559b4b5f08c078c117c77d064ca8928899f3
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2022-10-27 17:22:40 +0200

    Removing ts-ignore

--- examples/app-basic/src/module.ts
+++ examples/app-basic/src/module.ts
@@ -5,8 +5,6 @@ import { AppConfig } from './components/AppConfig';
 export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   title: 'Configuration',
   icon: 'cog',
-  // @ts-ignore - Would expect a Class component, however works absolutely fine with a functional one
-  // Implementation: https://github.com/grafana/grafana/blob/fd44c01675e54973370969dfb9e78f173aff7910/public/app/features/plugins/PluginPage.tsx#L157
   body: AppConfig,
   id: 'configuration',
 });
