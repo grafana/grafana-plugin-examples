0a74b564b0100bb3d4df1a31c9a3e5434a9a0678
Giuseppe Guerra <giuseppe@guerra.in>
2022-10-28 12:50:05 +0200

    HTTP backend plugin: Add mage target to build server

--- examples/datasource-http-backend/Magefile.go
+++ examples/datasource-http-backend/Magefile.go
@@ -4,9 +4,30 @@
 package main
 
 import (
+	"github.com/magefile/mage/mg"
+
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/sh"
+	"os"
+	"path"
 )
 
 // Default configures the default target.
 var Default = build.BuildAll
+
+// Server builds the example server inside cmd/server
+func Server() error {
+	os.Chdir(path.Join("cmd", "server"))
+	defer os.Chdir(path.Join("..", ".."))
+	if err := sh.Run("go", "build", "-v"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CleanAll runs the 'clean' target and also removes the example server binary in cmd/server
+func CleanAll() error {
+	mg.Deps(build.Clean)
+	return os.Remove(path.Join("cmd", "server", "server"))
+}
