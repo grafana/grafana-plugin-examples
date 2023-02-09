ae52dcda2e77f057fa3234b4b2086bd9141407f8
Jack Westbrook <jack.westbrook@gmail.com>
2023-02-09 12:17:20 +0100

    Wip

--- examples/app-basic/src/module.ts
+++ examples/app-basic/src/module.ts
@@ -2,9 +2,22 @@ import { AppPlugin } from '@grafana/data';
 import { App } from './components/App';
 import { AppConfig } from './components/AppConfig';
 
-export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
-  title: 'Configuration',
-  icon: 'cog',
-  body: AppConfig,
-  id: 'configuration',
-});
+export const plugin = new AppPlugin<{}>()
+  .setRootPage(App)
+  .addConfigPage({
+    title: 'Configuration',
+    icon: 'cog',
+    body: AppConfig,
+    id: 'configuration',
+  })
+  //@ts-ignore
+  .configureExtensionLink('declare-incident', (context: any, link: any) => {
+    if (context.panel.type === 'timeseries') {
+      return null;
+    }
+
+    return {
+      ...link,
+      title: 'dynamic title',
+    };
+  });
