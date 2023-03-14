b6d717b0e0bc6ee88234547a79bb9c94edbdeb41
Marcus Andersson <marcus.andersson@grafana.com>
2023-03-14 13:59:29 +0100

    draft of example on how to add link extension.

---
+++ examples/app-with-extensions/src/module.ts
@@ -0,0 +1,20 @@
+import { AppPlugin, PluginExtensionPlacements } from '@grafana/data';
+import { App } from './components/App';
+import { AppConfig } from './components/AppConfig';
+import pluginJson from 'plugin.json';
+
+export const plugin = new AppPlugin<{}>()
+  .setRootPage(App)
+  .addConfigPage({
+    title: 'Configuration',
+    icon: 'cog',
+    body: AppConfig,
+    id: 'configuration',
+  })
+  .configureExtensionLink({
+    title: 'Go to extensions app',
+    description: 'testing',
+    placement: PluginExtensionPlacements.DashboardPanelMenu,
+    path: `/a/${pluginJson.id}/`,
+    configure: (link, context) => link,
+  });
