0feb7fb55b011e9f977e0a7a18dae586c65ff395
Esteban Beltran <esteban@academo.me>
2023-07-28 16:26:38 +0200

    Update plugin.json files now

--- scripts/update-grafana-version.sh
+++ scripts/update-grafana-version.sh
@@ -5,8 +5,11 @@ GRAFANA_VERSION_TARGET=${1:-10.0.3}
 # fail this script if jq is not installed
 command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required but it's not installed.  Aborting."; exit 1; }
 
-dirs=$(find examples -type f -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \;)
 
+###############################################
+# Upgrade @grafana/* dependencies
+###############################################
+dirs=$(find examples -type f -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \;)
 
 # Specify the packages for version upgrade
 upgradePackages=(
@@ -50,14 +53,37 @@ for dir in $dirs; do
   if [ -f "package-lock.json" ]; then
     echo "package-lock.json exists, running npm install"
     npm install
-    npm upgrade $upgradeCommand
+    echo "upgrading packages with npm install $upgradeCommand"
+    npm install --save-exact $upgradeCommand
   fi
 
   # if a yarn.lock exists, run yarn install
   if [ -f "yarn.lock" ]; then
     echo "yarn.lock exists, running yarn install"
     yarn install
-    yarn upgrade $upgradeCommand
+    echo "upgrading packages with yarn upgrade $upgradeCommand"
+    yarn upgrade --exact $upgradeCommand
   fi
   popd || exit 1
 done
+
+###############################################
+# Upgrade plugin.json files
+###############################################
+
+
+plugin_files=$(find examples -type f -name 'plugin.json' -not -path '*/node_modules/*')
+
+# Iterate over each plugin.json file
+for file in $plugin_files; do
+    # Get the current value of dependencies.grafanaDependency using jq
+    current_dependency=$(jq -r '.dependencies.grafanaDependency' "$file")
+
+    # Modify the property to >$GRAFANA_VERSION_TARGET using jq and update the file
+    modified_dependency=$(echo "$current_dependency" | jq -n --arg target ">=$GRAFANA_VERSION_TARGET" '$target')
+    jq --argjson modified "$modified_dependency" '.dependencies.grafanaDependency = $modified' "$file" >"$file.tmp" && mv "$file.tmp" "$file"
+done
+
+###############################################
+# Upgrade docker-compose files
+###############################################
