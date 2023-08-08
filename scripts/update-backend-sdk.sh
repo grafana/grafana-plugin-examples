d8543a1f471fb6bd20b7fcbc4ce211077bcd4be4
Giuseppe Guerra <giuseppe@guerra.in>
2023-08-08 10:22:45 +0200

    Add script to update backend sdk

---
+++ scripts/update-backend-sdk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]; then
+  echo "Usage: $0 [vX.Y.Z]"
+  echo "(defaults to latest version if not specified)"
+  exit 0
+fi
+
+SDK_TARGET="$1"
+
+# Find go.mod files (backend plugins)
+files=$(find examples -type f -name "go.mod")
+
+base=$(pwd)
+
+# Iterate over each file and run `go get` to upgrade the grafana-plugin-sdk-go dependency
+for file in $files; do
+  echo "Upgrading grafana-plugin-sdk-go to ${SDK_TARGET:-latest} in $file"
+  cd $(dirname "$file")
+  if [ ! -z "$SDK_TARGET" ]; then
+    go get "github.com/grafana/grafana-plugin-sdk-go@$SDK_TARGET"
+  else
+    go get -u github.com/grafana/grafana-plugin-sdk-go
+  fi
+  go mod tidy
+  cd "$base"
+done
