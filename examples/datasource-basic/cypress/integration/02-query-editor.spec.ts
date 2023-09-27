5e6efa29171e65240676a8bcb82b9ba36067042f
Marcus Andersson <marcus.andersson@grafana.com>
2023-09-27 21:34:42 +0200

    Fixed failing tests.

--- examples/datasource-basic/cypress/integration/02-query-editor.spec.ts
+++ examples/datasource-basic/cypress/integration/02-query-editor.spec.ts
@@ -1,176 +1,150 @@
 import { e2e } from '@grafana/e2e';
 
 describe('querying datasource', () => {
-  beforeEach(() => {
-    e2e.flows.openDashboard({
-      uid: 'lYi_st-7z',
-      queryParams: {
-        from: new Date('2022-02-01 10:00:00Z').getTime(),
-        to: new Date('2022-02-01 12:00:00Z').getTime(),
-        'var-table': 'temperature_metrics',
-      },
-    });
-  });
+  const baseUrl = 'http://localhost:3000/d/lYi_st-7z/basic-datasource-example';
+  const from = new Date('2022-02-01 10:00:00Z').getTime();
+  const to = new Date('2022-02-01 12:00:00Z').getTime();
+  const variable = 'temperature_metrics';
 
-  describe('with query type: time series', () => {
+  describe('in timeseries panel', () => {
+    const panelId = 4;
+
+    beforeEach(() => {
+      cy.intercept('POST', '/api/ds/query*').as('query');
+      cy.visit(`${baseUrl}?viewPanel=${panelId}&from=${from}&to=${to}&var-table=${variable}`);
+    });
+
     it('should return data with one value/time field', () => {
-      const panel = 'Timeseries';
-      const data = [
-        { time: '2022-02-01 10:00:00', value: 25 },
-        { time: '2022-02-01 10:12:00', value: 22 },
-        { time: '2022-02-01 10:24:00', value: 19 },
-        { time: '2022-02-01 10:36:00', value: 23 },
-        { time: '2022-02-01 10:48:00', value: 22 },
-        { time: '2022-02-01 11:00:00', value: 22 },
-        { time: '2022-02-01 11:12:00', value: 18 },
-        { time: '2022-02-01 11:24:00', value: 26 },
-        { time: '2022-02-01 11:36:00', value: 24 },
-        { time: '2022-02-01 11:48:00', value: 20 },
+      const expectedData = [
+        { time: new Date('2022-02-01 10:00:00Z').getTime(), value: 25 },
+        { time: new Date('2022-02-01 10:12:00Z').getTime(), value: 22 },
+        { time: new Date('2022-02-01 10:24:00Z').getTime(), value: 19 },
+        { time: new Date('2022-02-01 10:36:00Z').getTime(), value: 23 },
+        { time: new Date('2022-02-01 10:48:00Z').getTime(), value: 22 },
+        { time: new Date('2022-02-01 11:00:00Z').getTime(), value: 22 },
+        { time: new Date('2022-02-01 11:12:00Z').getTime(), value: 18 },
+        { time: new Date('2022-02-01 11:24:00Z').getTime(), value: 26 },
+        { time: new Date('2022-02-01 11:36:00Z').getTime(), value: 24 },
+        { time: new Date('2022-02-01 11:48:00Z').getTime(), value: 20 },
       ];
 
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.PanelInspector.Data.content()
-        .should('be.visible')
-        .within(() => {
-          for (let index = 0; index < data.length; index++) {
-            const expected = data[index];
-            const timeIndex = index * 2;
-            const valueIndex = timeIndex + 1;
+      cy.wait('@query', { timeout: 50000 }).then((interception) => {
+        const data = getDataFromResponse(interception);
 
-            cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
-            cy.get('div[role=cell]').eq(valueIndex).contains(expected.value).should('be.visible');
-          }
+        for (let i = 0; i < expectedData.length; i++) {
+          const expected = expectedData[i];
+          const time = data.values[0][i];
+          const value = data.values[1][i];
+
+          expect(time).to.equal(expected.time);
+          expect(value).to.equal(expected.value);
+        }
+      });
+    });
+
+    describe('with macro: $__timeFilter(column)', () => {
+      it('should use specified time column in query', () => {
+        const executedQuery =
+          'SELECT * FROM temperature_metrics WHERE created >= datetime(2022-02-01T10:00:00Z) and created <= datetime(2022-02-01T12:00:00Z)';
+        const rawQuery = 'SELECT * FROM temperature_metrics WHERE $__timeFilter(created)';
+
+        cy.wait('@query').then((interception) => {
+          getRawQueryFromRequest(interception).should('eq', rawQuery);
+          getExecutedQueryFromResponse(interception).should('eq', executedQuery);
         });
+      });
     });
   });
 
-  describe('with query type: table', () => {
+  describe('in table panel', () => {
+    const panelId = 5;
+    const panel = 'Table';
+
+    beforeEach(() => {
+      cy.intercept('POST', '/api/ds/query*').as('query');
+      cy.visit(`${baseUrl}?viewPanel=${panelId}&from=${from}&to=${to}&var-table=${variable}`);
+    });
+
     it('should return data with time and two value fields', () => {
-      const panel = 'Table';
-      const data = [
-        { time: '2022-02-01 10:00:00', inside: 25, outside: 10 },
-        { time: '2022-02-01 10:12:00', inside: 22, outside: 8 },
-        { time: '2022-02-01 10:24:00', inside: 19, outside: 12 },
-        { time: '2022-02-01 10:36:00', inside: 23, outside: 9 },
-        { time: '2022-02-01 10:48:00', inside: 22, outside: 10 },
-        { time: '2022-02-01 11:00:00', inside: 22, outside: 11 },
-        { time: '2022-02-01 11:12:00', inside: 18, outside: 10 },
-        { time: '2022-02-01 11:24:00', inside: 26, outside: 9 },
-        { time: '2022-02-01 11:36:00', inside: 24, outside: 10 },
-        { time: '2022-02-01 11:48:00', inside: 20, outside: 9 },
+      const expectedData = [
+        { time: new Date('2022-02-01 10:00:00Z').getTime(), inside: 25, outside: 10 },
+        { time: new Date('2022-02-01 10:12:00Z').getTime(), inside: 22, outside: 8 },
+        { time: new Date('2022-02-01 10:24:00Z').getTime(), inside: 19, outside: 12 },
+        { time: new Date('2022-02-01 10:36:00Z').getTime(), inside: 23, outside: 9 },
+        { time: new Date('2022-02-01 10:48:00Z').getTime(), inside: 22, outside: 10 },
+        { time: new Date('2022-02-01 11:00:00Z').getTime(), inside: 22, outside: 11 },
+        { time: new Date('2022-02-01 11:12:00Z').getTime(), inside: 18, outside: 10 },
+        { time: new Date('2022-02-01 11:24:00Z').getTime(), inside: 26, outside: 9 },
+        { time: new Date('2022-02-01 11:36:00Z').getTime(), inside: 24, outside: 10 },
+        { time: new Date('2022-02-01 11:48:00Z').getTime(), inside: 20, outside: 9 },
       ];
 
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.PanelInspector.Data.content()
-        .should('be.visible')
-        .within(() => {
-          for (let index = 0; index < data.length; index++) {
-            const expected = data[index];
-            const timeIndex = index * 3;
-            const outsideIndex = timeIndex + 1;
-            const insideIndex = outsideIndex + 1;
+      cy.wait('@query', { timeout: 50000 }).then((interception) => {
+        const data = getDataFromResponse(interception);
 
-            cy.get('div[role=cell]').eq(timeIndex).contains(expected.time).should('be.visible');
-            cy.get('div[role=cell]').eq(outsideIndex).contains(expected.outside).should('be.visible');
-            cy.get('div[role=cell]').eq(insideIndex).contains(expected.inside).should('be.visible');
-          }
+        for (let i = 0; i < expectedData.length; i++) {
+          const expected = expectedData[i];
+          const time = data.values[0][i];
+          const outside = data.values[1][i];
+          const inside = data.values[2][i];
+
+          expect(time).to.equal(expected.time);
+          expect(outside).to.equal(expected.outside);
+          expect(inside).to.equal(expected.inside);
+        }
+      });
+    });
+
+    describe('with macro: $__timeFilter()', () => {
+      it('should use default time column in query', () => {
+        const executedQuery =
+          'SELECT * FROM temperature_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)';
+        const rawQuery = 'SELECT * FROM temperature_metrics WHERE $__timeFilter()';
+
+        cy.wait('@query').then((interception) => {
+          getRawQueryFromRequest(interception).should('eq', rawQuery);
+          getExecutedQueryFromResponse(interception).should('eq', executedQuery);
         });
-    });
-  });
-
-  describe('with macro: $__timeFilter()', () => {
-    it('should use default time column in query', () => {
-      const panel = 'Table';
-      const executedQuery =
-        'SELECT * FROM temperature_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)';
-      const rawQuery = 'SELECT * FROM temperature_metrics WHERE $__timeFilter()';
-
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.Drawer.General.title(`Inspect: ${panel}`).within(() => {
-        e2e.components.Tab.title('Query').should('be.visible').click();
-
-        // First verify that our raw query contains the macro
-        e2e.components.PanelInspector.Query.refreshButton().should('be.visible').click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys().contains('queries:').should('be.visible').parent().click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys().contains('0:').should('be.visible').parent().click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys()
-          .contains('rawQuery:')
-          .should('be.visible')
-          .parent()
-          .within(() => cy.get('span.json-formatter-string').contains(rawQuery).should('be.visible'));
-
-        // Then verify that the executed query contains the replacement for the macro
-        e2e.components.PanelInspector.Query.content().get('pre').contains(executedQuery).should('be.visible');
       });
     });
-  });
 
-  describe('with macro: $__timeFilter(column)', () => {
-    it('should use specified time column in query', () => {
-      const panel = 'Timeseries';
-      const executedQuery =
-        'SELECT * FROM temperature_metrics WHERE created >= datetime(2022-02-01T10:00:00Z) and created <= datetime(2022-02-01T12:00:00Z)';
-      const rawQuery = 'SELECT * FROM temperature_metrics WHERE $__timeFilter(created)';
+    describe('with template variable: $table', () => {
+      it('should apply variable value from query', () => {
+        // Check that datasource apply macro properly
+        const initialRawQuery = 'SELECT * FROM temperature_metrics WHERE $__timeFilter()';
+        const initialExecutedQuery =
+          'SELECT * FROM temperature_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)';
 
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.Drawer.General.title(`Inspect: ${panel}`).within(() => {
-        e2e.components.Tab.title('Query').should('be.visible').click();
+        cy.wait('@query').then((interception) => {
+          getRawQueryFromRequest(interception).should('eq', initialRawQuery);
+          getExecutedQueryFromResponse(interception).should('eq', initialExecutedQuery);
+        });
 
-        // First verify that our raw query contains the macro
-        e2e.components.PanelInspector.Query.refreshButton().should('be.visible').click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys().contains('queries:').should('be.visible').parent().click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys().contains('0:').should('be.visible').parent().click();
-        e2e.components.PanelInspector.Query.jsonObjectKeys()
-          .contains('rawQuery:')
-          .should('be.visible')
-          .parent()
-          .within(() => cy.get('span.json-formatter-string').contains(rawQuery).should('be.visible'));
+        // Changing template variable via query to 'battery_metrics'
+        cy.visit(`${baseUrl}?viewPanel=${panelId}&from=${from}&to=${to}&var-table=battery_metrics`);
 
-        // Then verify that the executed query contains the replacement for the macro
-        e2e.components.PanelInspector.Query.content().get('pre').contains(executedQuery).should('be.visible');
-      });
-    });
-  });
+        // Check that datasource apply macro properly after variable change
+        const updatedRawQuery = 'SELECT * FROM battery_metrics WHERE $__timeFilter()';
+        const updatedExecutedQuery =
+          'SELECT * FROM battery_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)';
 
-  describe('with template variable: $table', () => {
-    it('should apply variable value in query', () => {
-      const panel = 'Table';
-
-      // Verify that we are using the `temperature_metrics` template value.
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.Drawer.General.title(`Inspect: ${panel}`).within(() => {
-        e2e.components.Tab.title('Query').should('be.visible').click();
-        e2e.components.PanelInspector.Query.content()
-          .should('be.visible')
-          .get('pre')
-          .contains(
-            'SELECT * FROM temperature_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)'
-          )
-          .should('be.visible');
-      });
-
-      // Change value of the template variable to `battery_metrics`.
-      e2e.flows.openDashboard({
-        uid: 'lYi_st-7z',
-        queryParams: {
-          from: new Date('2022-02-01 10:00:00Z').getTime(),
-          to: new Date('2022-02-01 12:00:00Z').getTime(),
-          'var-table': 'battery_metrics',
-        },
-      });
-
-      // Verify that we are using the `battery_metrics` template value.
-      e2e.flows.openPanelMenuItem(e2e.flows.PanelMenuItems.Inspect, panel);
-      e2e.components.Drawer.General.title(`Inspect: ${panel}`).within(() => {
-        e2e.components.Tab.title('Query').should('be.visible').click();
-        e2e.components.PanelInspector.Query.content()
-          .should('be.visible')
-          .get('pre')
-          .contains(
-            'SELECT * FROM battery_metrics WHERE time >= datetime(2022-02-01T10:00:00Z) and time <= datetime(2022-02-01T12:00:00Z)'
-          )
-          .should('be.visible');
+        cy.wait('@query').then((interception) => {
+          getRawQueryFromRequest(interception).should('eq', updatedRawQuery);
+          getExecutedQueryFromResponse(interception).should('eq', updatedExecutedQuery);
+        });
       });
     });
   });
 });
+
+function getExecutedQueryFromResponse(interception: any) {
+  return cy.wrap(interception.response.body.results.A.frames[0].schema.meta.executedQueryString);
+}
+
+function getRawQueryFromRequest(interception: any) {
+  return cy.wrap(interception.request.body.queries[0].rawQuery);
+}
+
+function getDataFromResponse(interception: any) {
+  return interception.response.body.results.A.frames[0].data;
+}
