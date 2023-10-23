50cb0402d43df9724ee84135a2a19839ade2da35
Ivana Huckova <ivana.huckova@gmail.com>
2023-10-23 14:54:25 +0200

    Remove unused types for data source settings

--- examples/datasource-logs/src/types.ts
+++ examples/datasource-logs/src/types.ts
@@ -1,4 +1,3 @@
-import { DataSourceJsonData } from '@grafana/data';
 import { DataQuery } from '@grafana/schema'
 
 export interface MyQuery extends DataQuery {
@@ -11,17 +10,3 @@ export const DEFAULT_QUERY: Partial<MyQuery> = {
   // default limit is 100
   limit: 100,
 };
-
-/**
- * These are options configured for each DataSource instance
- */
-export interface MyDataSourceOptions extends DataSourceJsonData {
-  path?: string;
-}
-
-/**
- * Value that is used in the backend, but never sent over HTTP to the frontend
- */
-export interface MySecureJsonData {
-  apiKey?: string;
-}
