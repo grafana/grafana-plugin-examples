a9ea95c73ccc523d17392d5bf032b9e6d85962eb
Esteban Beltran <academo@users.noreply.github.com>
2023-12-08 15:37:57 +0100

    Update scenes library and required grafana dependencies  (#231)
    * Remove deprecated code from app scenes example
    
    * Update grafana and scenes

--- examples/app-with-scenes/src/components/App/App.tsx
+++ examples/app-with-scenes/src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { PluginPropsContext } from '../../utils/utils.plugin';
 import { Routes } from '../Routes';
