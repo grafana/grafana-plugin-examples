4717629ca32618f6cd5095563c75dde8517c30e4
Gabriel MABILLE <gabriel.mabille@grafana.com>
2024-01-31 22:41:35 +0100

    Fetch data

--- examples/app-with-rbac/src/pages/Patents.tsx
+++ examples/app-with-rbac/src/pages/Patents.tsx
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { testIds } from '../components/testIds';
 import { PluginPage } from '@grafana/runtime';
+import { InteractiveTable, Column, CellProps } from '@grafana/ui';
+
+interface Patent {
+  title: string;
+  inventors: string[];
+}
 
 export function Patents() {
-   return (
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [patents, setPatents] = useState<Patent[]>([]);
+
+  const columns = useMemo<Array<Column<Patent>>>(
+    () => [
+      {
+        id: 'title',
+        header: 'Patent title',
+      },
+      {
+        id: 'inventors',
+        header: 'Inventors',
+        cell: ({ cell: { value } }: CellProps<string[]>) => value.join(', '),
+      },
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const fetchPatents = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch('/api/plugins/grafana-appwithrbac-app/resources/patents', { method: 'GET' });
+
+        if (!response.ok) {
+          throw new Error('Request failed. Status ' + response.statusText);
+        }
+
+        const json = await response.json();
+        setPatents(json);
+      } catch (err: any) {
+        setError(err.message || 'Something went wrong with the request.');
+      }
+      setIsLoading(false);
+    };
+    fetchPatents();
+  }, []);
+
+  let content = <></>;
+
+  if (isLoading) {
+    content = <div> The page is loading </div>;
+  }
+
+  if (error) {
+    content = <div> An error happened while fetching the patents: {error} </div>;
+  }
+
+  if (!isLoading && !error) {
+    content = <InteractiveTable columns={columns} data={patents} getRowId={(row: Patent) => row.title} />;
+  }
+
+  return (
     <PluginPage>
-      <div data-testid={testIds.patents.container}>
-        This is the patents page, normally restricted to admins.
-      </div>
+      <div data-testid={testIds.patents.container}>This is the patents page, normally restricted to admins.</div>
+      {content}
     </PluginPage>
   );
 }
--- examples/app-with-rbac/src/pages/ResearchDocs.tsx
+++ examples/app-with-rbac/src/pages/ResearchDocs.tsx
@@ -1,13 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { testIds } from '../components/testIds';
 import { PluginPage } from '@grafana/runtime';
+import { InteractiveTable, Column, CellProps } from '@grafana/ui';
+
+interface Paper {
+  title: string;
+  authors: string[];
+}
 
 export function ResearchDocs() {
-   return (
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [papers, setPapers] = useState<Paper[]>([]);
+
+  const columns = useMemo<Array<Column<Paper>>>(
+    () => [
+      {
+        id: 'title',
+        header: 'Patent title',
+      },
+      {
+        id: 'authors',
+        header: 'authors',
+        cell: ({ cell: { value } }: CellProps<string[]>) => value.join(', '),
+      },
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const fetchPapers = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch('/api/plugins/grafana-appwithrbac-app/resources/papers', { method: 'GET' });
+
+        if (!response.ok) {
+          throw new Error('Request failed. Status ' + response.statusText);
+        }
+
+        const json = await response.json();
+        setPapers(json);
+      } catch (err: any) {
+        setError(err.message || 'Something went wrong with the request.');
+      }
+      setIsLoading(false);
+    };
+    fetchPapers();
+  }, []);
+
+  let content = <></>;
+
+  if (isLoading) {
+    content = <div> The page is loading </div>;
+  }
+
+  if (error) {
+    content = <div> An error happened while fetching the research papers: {error} </div>;
+  }
+
+  if (!isLoading && !error) {
+    content = <InteractiveTable columns={columns} data={papers} getRowId={(row: Paper) => row.title} />;
+  }
+
+  return (
     <PluginPage>
       <div data-testid={testIds.researchDocs.container}>
-        This is the research docs page, normally allowed to viewers.
+        This is the research papers page, normally allowed to viewers.
       </div>
+      {content}
     </PluginPage>
   );
 }
