1ff9b637bf7fdace5b112b1f47fe27cd35f640be
Marcus Andersson <marcus.andersson@grafana.com>
2024-03-01 16:06:53 +0100

    removed tablist verification since it is out of context for the plugin.

--- examples/app-basic/tests/appNavigation.spec.ts
+++ examples/app-basic/tests/appNavigation.spec.ts
@@ -7,10 +7,9 @@ test.describe('navigating app', () => {
     await expect(page.getByText('This is page one.')).toBeVisible();
   });
 
-  test('page two should have tablist menu on left side', async ({ page }) => {
+  test('page two should render successfully', async ({ page }) => {
     await page.goto(`/a/${pluginJson.id}/two`);
     await expect(page.getByText('This is page two.')).toBeVisible();
-    await expect(page.getByRole('tablist')).toBeVisible();
   });
 
   test('page two should support an id parameter', async ({ page }) => {
@@ -18,18 +17,15 @@ test.describe('navigating app', () => {
     await expect(page.getByText('ID: 123456')).toBeVisible();
   });
 
-  test('page three should render sucessfully as full-width without leftside tab menu', async ({ page }) => {
+  test('page three should render sucessfully', async ({ page }) => {
     // wait for page to successfully render
     await page.goto(`/a/${pluginJson.id}/one`);
     await expect(page.getByText('This is page one.')).toBeVisible();
-    await expect(page.getByRole('tablist')).toBeVisible();
 
     // navigating to page four with full width layout without sidebar menu
     await page.getByText('Full-width page example').click();
-    await expect(page.getByRole('tablist')).not.toBeVisible();
 
     // navigate back to page one and verify tat sidebar menu is back
     await page.getByText('Back').click();
-    await expect(page.getByRole('tablist')).toBeVisible();
   });
 });
