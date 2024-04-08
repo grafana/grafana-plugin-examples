208855142a6703eb89ca6a9d9230700e50b1ec67
Erik Sundell <erik.sundell87@gmail.com>
2024-04-08 13:36:11 +0200

    add e2e tests

--- examples/panel-basic/src/types.ts
+++ examples/panel-basic/src/types.ts
@@ -1,5 +1,9 @@
 import { VizLegendOptions } from '@grafana/schema';
 
+type SeriesSize = 'sm' | 'md' | 'lg';
+
 export interface SimpleOptions {
+  showSeriesCount: boolean;
+  seriesCountSize: SeriesSize;
   legend: VizLegendOptions;
 }
