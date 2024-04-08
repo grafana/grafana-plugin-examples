1b13b8230ce168dd73def319229c93ffae21f8b0
Erik Sundell <erik.sundell87@gmail.com>
2024-04-08 15:42:09 +0200

    add playwright and plugin-e2e

--- examples/datasource-http/src/ConfigEditor.tsx
+++ examples/datasource-http/src/ConfigEditor.tsx
@@ -1,16 +1,76 @@
+import React, { ChangeEvent } from 'react';
+import { DataSourceHttpSettings, InlineField, Input, SecretInput } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { DataSourceHttpSettings } from '@grafana/ui';
-import React from 'react';
-import { MyDataSourceOptions } from './types';
+import { MyDataSourceOptions, MySecureJsonData } from './types';
 
-interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
+interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions, MySecureJsonData> {}
 
-export const ConfigEditor: React.FC<Props> = ({ onOptionsChange, options }) => {
+export function ConfigEditor(props: Props) {
+  const { onOptionsChange, options } = props;
+  const { jsonData, secureJsonFields, secureJsonData } = options;
+
+  const onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...jsonData,
+        path: event.target.value,
+      },
+    });
+  };
+
+  // Secure field (only sent to the backend)
+  const onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        apiKey: event.target.value,
+      },
+    });
+  };
+
+  const onResetAPIKey = () => {
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        apiKey: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        apiKey: '',
+      },
+    });
+  };
+
   return (
-    <DataSourceHttpSettings
-      defaultUrl="https://api.example.com"
-      dataSourceConfig={options}
-      onChange={onOptionsChange}
-    />
+    <>
+      <DataSourceHttpSettings
+        defaultUrl="https://api.example.com"
+        dataSourceConfig={options}
+        onChange={onOptionsChange}
+      />
+      <InlineField label="Path" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
+        <Input
+          id="config-editor-path"
+          onChange={onPathChange}
+          value={jsonData.path}
+          placeholder="Enter the path, e.g. /api/v1"
+          width={40}
+        />
+      </InlineField>
+      <InlineField label="API Key" labelWidth={14} interactive tooltip={'Secure json field (backend only)'}>
+        <SecretInput
+          required
+          id="config-editor-api-key"
+          isConfigured={secureJsonFields.apiKey}
+          value={secureJsonData?.apiKey}
+          placeholder="Enter your API key"
+          width={40}
+          onReset={onResetAPIKey}
+          onChange={onAPIKeyChange}
+        />
+      </InlineField>
+    </>
   );
-};
+}
