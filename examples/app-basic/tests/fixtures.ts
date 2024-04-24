e10df2d832104cd2843b584248b869974a1a4663
Marcus Andersson <marcus.andersson@grafana.com>
2024-04-24 11:09:01 +0200

    Examples: cleaning up tests for app basic a bit more (#279)
    * cleaning up the e2e app tests a bit more.
    
    * removed pluginJson import in test file.
    
    * using the gotoAppConfigPage instead of creating an app config page class.
    
    * making gotoPage return type a bit more explicit.

--- examples/app-basic/tests/fixtures.ts
+++ examples/app-basic/tests/fixtures.ts
@@ -1,18 +1,25 @@
-import { AppConfigPage, test as base } from '@grafana/plugin-e2e';
+import { AppConfigPage, AppPage, test as base } from '@grafana/plugin-e2e';
 import pluginJson from '../src/plugin.json';
 
-type AppTestFixture = { appConfigPage: AppConfigPage };
+type AppTestFixture = {
+  appConfigPage: AppConfigPage;
+  gotoPage: (path?: string) => Promise<AppPage>;
+};
 
 export const test = base.extend<AppTestFixture>({
-  appConfigPage: async ({ page, selectors, grafanaVersion, request }, use, testInfo) => {
-    const configPage = new AppConfigPage(
-      { page, selectors, grafanaVersion, request, testInfo },
-      {
-        pluginId: pluginJson.id,
-      }
-    );
-    await configPage.goto();
+  appConfigPage: async ({ gotoAppConfigPage }, use) => {
+    const configPage = await gotoAppConfigPage({
+      pluginId: pluginJson.id,
+    });
     await use(configPage);
+  },
+  gotoPage: async ({ gotoAppPage }, use) => {
+    await use((path) =>
+      gotoAppPage({
+        path,
+        pluginId: pluginJson.id,
+      })
+    );
   },
 });
 
