9e2fee8abe6b9a2e9f264f861366fd69b7d8ac6b
Marcus Andersson <marcus.andersson@grafana.com>
2024-04-24 10:14:09 +0200

    cleaning up the e2e app tests a bit more.

--- examples/app-basic/tests/fixtures.ts
+++ examples/app-basic/tests/fixtures.ts
@@ -1,7 +1,10 @@
-import { AppConfigPage, test as base } from '@grafana/plugin-e2e';
+import { AppConfigPage, AppPage, test as base } from '@grafana/plugin-e2e';
 import pluginJson from '../src/plugin.json';
 
-type AppTestFixture = { appConfigPage: AppConfigPage };
+type AppTestFixture = {
+  appConfigPage: AppConfigPage;
+  gotoPage: (path?: string) => Promise<AppPage>;
+};
 
 export const test = base.extend<AppTestFixture>({
   appConfigPage: async ({ page, selectors, grafanaVersion, request }, use, testInfo) => {
@@ -14,6 +17,13 @@ export const test = base.extend<AppTestFixture>({
     await configPage.goto();
     await use(configPage);
   },
+  gotoPage: ({ gotoAppPage }, use) =>
+    use((path) =>
+      gotoAppPage({
+        path,
+        pluginId: pluginJson.id,
+      })
+    ),
 });
 
 export { expect } from '@grafana/plugin-e2e';
