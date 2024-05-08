68718e0a765b55212a38f1eaa3c6c875cc838845
Marcus Andersson <marcus.andersson@grafana.com>
2024-05-08 11:36:30 +0200

    AppWithDashboard: updating tests to use navigation function (#281)
    * changed tests to use a navigation function.
    
    * added missing file.

--- examples/app-with-dashboards/tests/appNavigation.spec.ts
+++ examples/app-with-dashboards/tests/appNavigation.spec.ts
@@ -1,20 +1,19 @@
-import pluginJson from '../src/plugin.json';
-import { test, expect } from '@grafana/plugin-e2e';
+import { test, expect } from './fixtures';
 
 test.describe('navigating app', () => {
-  test('should render home page successfully', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}`);
+  test('should render home page successfully', async ({ gotoPage, page }) => {
+    await gotoPage('/');
     await expect(page.getByText('Hello and welcome to our demo app!')).toBeVisible();
   });
 
-  test('should be possible to navigate to example dashboard 1', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}`);
+  test('should be possible to navigate to example dashboard 1', async ({ gotoPage, page }) => {
+    await gotoPage('/');
     await page.getByText('Example dashboard 1').click();
     await expect(page).toHaveURL(/\/d\/Av57mRHVz$/);
   });
 
-  test('should be possible to navigate to example dashboard 2', async ({ page }) => {
-    await page.goto(`/a/${pluginJson.id}`);
+  test('should be possible to navigate to example dashboard 2', async ({ gotoPage, page }) => {
+    await gotoPage('/');
     await page.getByText('Example dashboard 2').click();
     await expect(page).toHaveURL(/\/d\/ND1Bfw3VcNGg$/);
   });
