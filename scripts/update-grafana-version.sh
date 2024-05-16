af85e40061eb91a8d839ff2a345ddac36556f9f3
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2024-05-16 11:04:10 +0200

    Run create-plugin update for panel-plotly plugin example (#291)
    * Run create-plugin update for panel-plotly plugin example
    
    * remove panel plotly hardcoded path from the script
    
    * remove commented lines

--- scripts/update-grafana-version.sh
+++ scripts/update-grafana-version.sh
@@ -3,21 +3,21 @@
 GRAFANA_VERSION_TARGET=${1:-10.3.1}
 
 dirs=$(find examples -type f -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \;)
+#dirs=('examples/panel-plotly' 'examples/panel-scatterplot' 'examples/panel-visx')
 for plugin in $dirs; do
     if [ -d "$plugin" ]; then  # Check if it is indeed a directory
         echo "Processing plugin folder: $plugin"
         # Run the npx command in the plugin directory
-        (cd "$plugin" && npx @grafana/create-plugin@latest update --force && npm install (cd "$plugin" && npx @grafana/create-plugin@latest update --force && npm install && rm -rf node_modules))
+        (cd "$plugin" && npx @grafana/create-plugin@latest update --force && npm install && rm -rf node_modules)
     fi
 done
 
 ###############################################
 # Upgrade plugin.json files
 ###############################################
-
+get_newer_version () { printf "%s\n" "$@" | sort --version-sort | tail -1 ; }
 
 plugin_files=$(find examples -type f -name 'plugin.json' -not -path '*/node_modules/*')
-
 # Iterate over each plugin.json file
 for file in $plugin_files; do
     # Get the current value of dependencies.grafanaDependency using jq
@@ -32,20 +32,21 @@ done
 # Upgrade docker-compose files
 ###############################################
 # Find the docker-compose.yaml files excluding node_modules
-files=$(find examples -maxdepth 2 -type f -name "docker-compose.yaml" -not -path "*node_modules*")
+files=$(find examples -type f -name "docker-compose.yaml" -not -path "*node_modules*")
 # Make a wrapper to support Linux and MacOS
 case $(sed --help 2>&1) in
   *GNU*) sed_i () { sed -i "$@"; };;
   *) sed_i () { sed -i '' "$@"; };;
 esac
 
-get_newer_version () { printf "%s\n" "$@" | sort --version-sort | tail -1 ; }
+
 # Iterate over each file and modify the grafana_version field
 for file in $files; do
+    current_verson=$(sed -n 's|grafana_version: ${GRAFANA_VERSION:-\([0-9]*.[0-9]*.[0-9]*\)}|\1|p' "$file" | xargs)
+    target_version=$(get_newer_version "$current_verson" "$GRAFANA_VERSION_TARGET")
     # Check if the file contains the grafana_version field
     if grep -q "grafana_version:" "$file"; then
         # Replace the grafana_version field with the target version
-        # sed -i "s/grafana_version:.*/grafana_version: ${GRAFANA_VERSION_TARGET}/" "$file"
         sed_i "s/\(grafana_version: \)\${GRAFANA_VERSION:-[^}]*}/\1\${GRAFANA_VERSION:-$target_version}/" "$file"
         echo "Modified $file"
     fi
