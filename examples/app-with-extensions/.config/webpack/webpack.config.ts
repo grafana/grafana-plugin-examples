cf802d7befc642abe06493a3688a70798b75fe76
Levente Balogh <balogh.levente.hu@gmail.com>
2024-06-03 09:02:35 +0200

    App with extensions: set extensions meta-info manually (#306)
    * feat: add extensions meta-info manually
    
    * feat: update the create-plugin version (stop meta-info autogeneration)

--- examples/app-with-extensions/.config/webpack/webpack.config.ts
+++ examples/app-with-extensions/.config/webpack/webpack.config.ts
@@ -12,7 +12,6 @@ import LiveReloadPlugin from 'webpack-livereload-plugin';
 import path from 'path';
 import ReplaceInFileWebpackPlugin from 'replace-in-file-webpack-plugin';
 import { Configuration } from 'webpack';
-import { GrafanaPluginMetaExtractor } from '@grafana/plugin-meta-extractor';
 
 import { getPackageJson, getPluginJson, hasReadme, getEntries, isWSL } from './utils';
 import { SOURCE_DIR, DIST_DIR } from './constants';
@@ -141,7 +140,6 @@ const config = async (env): Promise<Configuration> => {
     },
 
     plugins: [
-      new GrafanaPluginMetaExtractor(),
       new CopyWebpackPlugin({
         patterns: [
           // If src/README.md exists use it; otherwise the root README
