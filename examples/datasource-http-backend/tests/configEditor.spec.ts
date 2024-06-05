ae32f72a90de3daab76cbcda7afea0e0554dc4f6
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2024-06-05 16:36:31 +0200

    WIP

---
+++ examples/datasource-http-backend/tests/configEditor.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@grafana/plugin-e2e';
+import { MyDataSourceOptions, MySecureJsonData } from '../src/types';
+
+test('"Save & test" should be successful when configuration is valid', async ({
+  createDataSourceConfigPage,
+  readProvisionedDataSource,
+  selectors,
+  page,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
+  const configPage = await createDataSourceConfigPage({ type: ds.type });
+  console.log('configPage', configPage);
+  const healthCheckPath = `${selectors.apis.DataSource.proxy(
+    configPage.datasource.uid,
+    configPage.datasource.id.toString()
+  )}/health`;
+  console.log('healthCheckPath', healthCheckPath);
+  await page.route(healthCheckPath, async (route) => await route.fulfill({ status: 200, body: 'OK' }));
+  await expect(configPage.saveAndTest({ path: healthCheckPath })).toBeOK();
+});
+
+test('"Save & test" should display error alert box when config is valid', async ({
+  createDataSourceConfigPage,
+  readProvisionedDataSource,
+  selectors,
+}) => {
+  const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
+  const configPage = await createDataSourceConfigPage({ type: ds.type });
+  const healthCheckPath = `${selectors.apis.DataSource.proxy(
+    configPage.datasource.uid,
+    configPage.datasource.id.toString()
+  )}/health`;
+  await expect(configPage.saveAndTest({ path: healthCheckPath })).not.toBeOK();
+  await expect(configPage).toHaveAlert('error');
+});
