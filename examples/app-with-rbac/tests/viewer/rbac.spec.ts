6ab9c65195cafaebe644458ad135a8ad33db398e
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2024-06-21 12:01:52 +0200

    WIP

--- examples/app-with-rbac/tests/viewer/rbac.spec.ts
+++ examples/app-with-rbac/tests/viewer/rbac.spec.ts
@@ -3,12 +3,10 @@ import { ROUTES } from '../../src/constants';
 
 test.describe('navigating app for Viewer', () => {
   test('Page patents should not be visible for viewer', async ({ gotoPage, page }) => {
-    // wait for page to successfully render
     await gotoPage(`/${ROUTES.Patents}`);
     await expect(page.getByText('Normally restricted to Administrators')).not.toBeVisible();
   });
   test('Page research docs should be visible for viewer', async ({ gotoPage, page }) => {
-    // wait for page to successfully render
     await gotoPage(`/${ROUTES.ResearchDocs}`);
     await expect(page.getByText('Normally accessible to anyone')).toBeVisible();
   });
