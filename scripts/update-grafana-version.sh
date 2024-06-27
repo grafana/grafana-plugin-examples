4241aecbe1d060aa55fbdc8d51a8322689bff907
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2024-06-27 16:04:30 +0200

    update-grafana-version script was updated and automatic update workflow

--- scripts/update-grafana-version.sh
+++ scripts/update-grafana-version.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 #
-GRAFANA_VERSION_TARGET=${1:-10.3.3}
 
-# dirs=$(find examples -type f -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \;)
-# #dirs=('examples/panel-plotly' 'examples/panel-scatterplot' 'examples/panel-visx')
-# for plugin in $dirs; do
-#     if [ -d "$plugin" ]; then  # Check if it is indeed a directory
-#         echo "Processing plugin folder: $plugin"
-#         # Run the npx command in the plugin directory
-#         (cd "$plugin" && npx @grafana/create-plugin@latest update --force && npm install && rm -rf node_modules)
-#     fi
-# done
+dirs=$(find examples -type f -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \;)
+#dirs=('examples/panel-plotly' 'examples/panel-scatterplot' 'examples/panel-visx')
+for plugin in $dirs; do
+    if [ -d "$plugin" ]; then  # Check if it is indeed a directory
+        echo "Processing plugin folder: $plugin"
+        # Run the npx command in the plugin directory
+        (cd "$plugin" && npx @grafana/create-plugin@latest update --force && npm install && rm -rf node_modules)
+    fi
+done
+
+# Get the target version from the updated package.json file
+GRAFANA_TARGET_VERSION=$(jq -r '.dependencies["@grafana/data"]' ./examples/app-basic/package.json)
+echo "Upgrading to Grafana version: $GRAFANA_TARGET_VERSION"
 
 ###############################################
 # Upgrade plugin.json files
 ###############################################
 get_newer_version () { printf "%s\n" "$@" | sort --version-sort | tail -1 ; }
 
-# plugin_files=$(find examples -type f -name 'plugin.json' -not -path '*/node_modules/*')
-# # Iterate over each plugin.json file
-# for file in $plugin_files; do
-#     # Get the current value of dependencies.grafanaDependency using jq
-#     current_dependency=$(jq -r '.dependencies.grafanaDependency' "$file")
-#     target_version=$(get_newer_version "$(echo $current_dependency | sed -n 's|[=<>~!]*\([0-9]*.[0-9]*.[0-9]*\)|\1|p')" "$GRAFANA_VERSION_TARGET")
-#     # Modify the property to >$GRAFANA_VERSION_TARGET using jq and update the file
-#     modified_dependency=$(echo "$current_dependency" | jq -n --arg target ">=$target_version" '$target')
-#     jq --argjson modified "$modified_dependency" '.dependencies.grafanaDependency = $modified' "$file" >"$file.tmp" && mv "$file.tmp" "$file"
-# done
+plugin_files=$(find examples -type f -name 'plugin.json' -not -path '*/node_modules/*')
+# Iterate over each plugin.json file
+for file in $plugin_files; do
+    # Get the current value of dependencies.grafanaDependency using jq
+    current_dependency=$(jq -r '.dependencies.grafanaDependency' "$file")
+    target_version=$(get_newer_version "$(echo $current_dependency | sed -n 's|[=<>~!]*\([0-9]*.[0-9]*.[0-9]*\)|\1|p')" "$GRAFANA_VERSION_TARGET")
+    # Modify the property to >$GRAFANA_VERSION_TARGET using jq and update the file
+    modified_dependency=$(echo "$current_dependency" | jq -n --arg target ">=$target_version" '$target')
+    jq --argjson modified "$modified_dependency" '.dependencies.grafanaDependency = $modified' "$file" >"$file.tmp" && mv "$file.tmp" "$file"
+done
 
 ###############################################
 # Upgrade docker-compose files
@@ -48,7 +51,6 @@ for file in $files; do
     if grep -q "grafana_version:" "$file"; then
         # Replace the grafana_version field with the target version
         sed_i "s/\(grafana_version: \)\${GRAFANA_VERSION:-[^}]*}/\1\${GRAFANA_VERSION:-$target_version}/" "$file"
-        echo "Modified $file"
     fi
 done
 
@@ -57,4 +59,4 @@ done
 # Upgrade grafana-plugin-sdk-go to latest version
 ###############################################
 
-#./scripts/update-backend-sdk.sh
+./scripts/update-backend-sdk.sh
