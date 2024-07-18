5d3e1d1bdec0fe80a87e760e527d3fb318707c38
Jack Westbrook <jack.westbrook@gmail.com>
2024-07-18 12:06:00 +0200

    feat(rbac): wip - migrate example to use new rbac runtime function

--- examples/app-with-rbac/src/components/App/App.tsx
+++ examples/app-with-rbac/src/components/App/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ROUTES } from '../../constants';
+import { hasPermission } from '@grafana/runtime';
 import { Hello, Patents, ResearchDocs } from '../../pages';
 import { prefixRoute } from '../../utils/utils.routing';
-// @ts-ignore
-import { contextSrv } from 'grafana/app/core/core';
 
 export function App() {
   let routes = [<Route exact path={prefixRoute(ROUTES.Hello)} component={Hello} key="hello" />];
-  if (contextSrv.hasPermission('grafana-appwithrbac-app.papers:read')) {
+  if (hasPermission('grafana-appwithrbac-app.papers:read')) {
     routes.push(<Route exact path={prefixRoute(ROUTES.ResearchDocs)} component={ResearchDocs} key="documents" />);
   }
-  if (contextSrv.hasPermission('grafana-appwithrbac-app.patents:read')) {
+  if (hasPermission('grafana-appwithrbac-app.patents:read')) {
     routes.push(<Route exact path={prefixRoute(ROUTES.Patents)} component={Patents} key="patents" />);
   }
 
-  return (
-    <Switch>
-      {routes}
-    </Switch>
-  );
+  return <Switch>{routes}</Switch>;
 }
