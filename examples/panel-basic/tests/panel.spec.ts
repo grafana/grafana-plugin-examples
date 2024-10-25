6b407fa1b5755b675ac5b0f99977fb049cac05f5
Erik Sundell <erik.sundell87@gmail.com>
2024-10-25 12:10:12 +0200

    Fix panel test (#409)
    * fix panel test
    
    * reverse order
    
    * fix panel name

--- examples/panel-basic/tests/panel.spec.ts
+++ examples/panel-basic/tests/panel.spec.ts
@@ -1,3 +1,4 @@
+import * as semver from 'semver';
 import { test, expect } from '@grafana/plugin-e2e';
 
 test('should display "No data" in case panel data is empty', async ({
@@ -14,6 +15,7 @@ test('should display series counter when "Show series counter" option is enabled
   readProvisionedDataSource,
   page,
   selectors,
+  grafanaVersion,
 }) => {
   const ds = await readProvisionedDataSource({ fileName: 'datasources.yml', name: 'TestData DB' });
   await panelEditPage.datasource.set(ds.name);
@@ -21,9 +23,12 @@ test('should display series counter when "Show series counter" option is enabled
   await panelEditPage.collapseSection('Basic Panel');
   await panelEditPage.refreshPanel();
   await expect(page.getByTestId('simple-panel-series-counter')).not.toBeVisible();
-  const showSeriesSwitch = panelEditPage
-    .getByGrafanaSelector(selectors.components.PanelEditor.OptionsPane.fieldLabel('Basic Panel Show series counter'))
-    .getByLabel('Toggle switch');
-  await showSeriesSwitch.click();
+  const seriesCounterLabel = panelEditPage.getByGrafanaSelector(
+    selectors.components.PanelEditor.OptionsPane.fieldLabel('Basic Panel Show series counter')
+  );
+  const switchField = semver.gte(grafanaVersion, '11.4.0')
+    ? seriesCounterLabel.getByRole('switch')
+    : seriesCounterLabel.getByLabel('Toggle switch');
+  await switchField.click({ force: true });
   await expect(page.getByTestId('simple-panel-series-counter')).toBeVisible();
 });
