e653bddd7046daa7d9b1182c677e48891cba74d0
Andres Martinez Gotor <andres.martinez@grafana.com>
2024-10-29 15:18:04 +0100

    Sample usage for user storage

--- examples/panel-basic/src/components/SimplePanel/SimplePanel.tsx
+++ examples/panel-basic/src/components/SimplePanel/SimplePanel.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PanelProps } from '@grafana/data';
-import { TimeSeries, TooltipPlugin, TooltipDisplayMode, ZoomPlugin } from '@grafana/ui';
+import { TimeSeries, TooltipPlugin, TooltipDisplayMode, ZoomPlugin, IconButton } from '@grafana/ui';
 import { SimpleOptions } from '../../types';
 import { testIds } from '../testIds';
-import { PanelDataErrorView } from '@grafana/runtime';
+import { PanelDataErrorView, getItem, setItem } from '@grafana/runtime';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
@@ -19,9 +19,24 @@ export function SimplePanel({
   fieldConfig,
   id,
 }: Props) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    getItem(`basic-panel.favorite`).then((value) => {
+      console.log('got value from storage', value);
+      setIsFavorite(value === 'true');
+    });
+  }, []);
+
   if (data.series.length === 0) {
     return <PanelDataErrorView fieldConfig={fieldConfig} panelId={id} data={data} needsStringField />;
   }
+
+  const onFavoriteClick = () => {
+    const newFavorite = !isFavorite;
+    setIsFavorite(newFavorite);
+    setItem(`basic-panel.favorite`, newFavorite.toString());
+  };
 
   return (
     <div data-testid={testIds.panel.container}>
@@ -30,6 +45,7 @@ export function SimplePanel({
           <div data-testid="simple-panel-series-counter">Number of series: {data.series.length}</div>
         )}
       </div>
+      <IconButton name={isFavorite ? 'favorite' : 'star'} aria-label="favorite" onClick={onFavoriteClick} />
       <TimeSeries
         width={width}
         height={height}
