accaf11206bfcc93b92fbce2b5191f3bd0ff8669
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-02-11 08:58:10 +0100

    Chore: fix ESLint in app-with-extensions

--- examples/app-with-extensions/src/utils.ts
+++ examples/app-with-extensions/src/utils.ts
@@ -1,4 +1,4 @@
-import { DataQuery } from '@grafana/data';
+import { DataQuery } from '@grafana/schema';
 
 export function selectQuery(target: DataQuery): void {
   alert(`You selected query "${target.refId}"`);
