a33a775b4c5861ff9ac1b1236a6e901999dd8ebb
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2025-04-08 14:21:17 +0200

    Feat: Adding signed commits flag to create PR action (#504)
    * Adding signed commits flag to create PR action
    
    * making the selectors more precise

--- examples/app-with-backend/tests/appCallingBackend.spec.ts
+++ examples/app-with-backend/tests/appCallingBackend.spec.ts
@@ -1,15 +1,16 @@
+import { testIds } from "../src/components/testIds";
 import { test, expect } from "./fixtures";
 
 test("page one should successfully call backend and display reply", async ({
   pageOne,
   page,
 }) => {
-  await expect(page.getByText("pong")).toBeVisible();
+  await expect(page.getByTestId(testIds.pageOne.ping).getByText("pong")).toBeVisible();
 });
 
 test("page one should succesfully check health and display status", async ({
   pageOne,
   page,
-}) => {
-  await expect(page.getByText("OK")).toBeVisible();
+}) => {  
+  await expect(page.getByTestId(testIds.pageOne.health).getByText("OK")).toBeVisible();
 });
